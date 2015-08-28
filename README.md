dc056abec7d07a7953935b7891eb7b1a991b3f4d
daniel.plominski <daniel@plominski.eu>
2015-08-28 23:37:58 +0200

    Version: 0.1

---
+++ README.md
@@ -0,0 +1,44 @@
+
+Background
+==========
+Wrapper Script for Dtrace Scripts
+
+based on:
+* [FreeBSD 10: IPv4 VPN Relay (IPsec entry/OpenVPN middle/OpenVPN exit node) mit Jails für Roadwarrior](https://blog.plitc.eu/2014/freebsd-10-ipv4-vpn-relay-ipsec-entryopenvpn-middleopenvpn-exit-node-mit-jails/)
+
+WARNING
+=======
+
+
+Dependencies
+============
+* FreeBSD
+   * [racoon/ipsec-tools](https://www.freshports.org/security/ipsec-tools/)
+   * [openvpn](https://www.freshports.org/security/openvpn/)
+
+Features
+========
+* FreeBSD
+  * ipsec connection
+
+Platform
+========
+* FreeBSD 10+
+* FreeNAS 9.3+ (currently not supported)
+
+Usage
+=====
+    # ./easy_dtrace.sh
+
+Screencast
+==========
+
+[![github plitc easy_ipsec](https://img.youtube.com/vi/GX6whhD096Y/0.jpg)](https://www.youtube.com/watch?v=GX6whhD096Y)
+
+Errata
+======
+* 28.08.2015:
+```
+
+```
+
---
+++ easy_dtrace.sh
@@ -0,0 +1,223 @@
+#!/bin/sh
+
+### LICENSE - (BSD 2-Clause) // ###
+#
+# Copyright (c) 2015, Daniel Plominski (Plominski IT Consulting)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+### // LICENSE - (BSD 2-Clause) ###
+
+### ### ### PLITC // ### ### ###
+
+### stage0 // ###
+OSVERSION=$(uname)
+FREENAS=$(uname -a | grep -c "ixsystems.com")
+JAILED=$(sysctl -a | grep -c "security.jail.jailed: 1")
+MYNAME=$(whoami)
+DATE=$(date +%Y%m%d-%H%M)
+#
+PRG="$0"
+#// need this for relative symlinks
+while [ -h "$PRG" ] ;
+do
+   PRG=$(readlink "$PRG")
+done
+DIR=$(dirname "$PRG")
+#
+ADIR="$PWD"
+#
+#// spinner
+spinner()
+{
+   local pid=$1
+   local delay=0.01
+   local spinstr='|/-\'
+   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+         local temp=${spinstr#?}
+         printf " [%c]  " "$spinstr"
+         local spinstr=$temp${spinstr%"$temp"}
+         sleep $delay
+         printf "\b\b\b\b\b\b"
+   done
+   printf "    \b\b\b\b"
+}
+#
+#// function cleanup tmp
+cleanup(){
+   rm -rf /tmp/easy_dtrace*
+}
+### // stage0 ###
+
+case "$1" in
+'freebsd')
+### stage1 // ###
+case $OSVERSION in
+FreeBSD)
+### stage2 // ###
+#
+### // stage2 ###
+
+### stage3 // ###
+if [ "$MYNAME" = "root" ]
+then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+if [ "$JAILED" = "0" ]
+then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] Run this script on the FreeBSD HOST"
+   exit 1
+fi
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+if [ "$FREENAS" = "1" ]
+then
+   : # dummy
+   : # dummy
+   echo "[ERROR] FreeBSD only support"
+   exit 1
+else
+   : # dummy
+fi
+
+#CHECKPKGSCREEN=$(pkg info | grep -c "screen-")
+#if [ "$CHECKPKGSCREEN" = "0" ]
+#then
+#   : # dummy
+#   : # dummy
+#   echo "[ERROR] You need sysutils/screen"
+#   exit 1
+#fi
+
+
+
+### ### ### ### ### ### ### ### ###
+cleanup
+### ### ### ### ### ### ### ### ###
+echo "" # printf
+printf "\033[1;31measy_dtrace for FreeBSD finished.\033[0m\n"
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+   ;;
+*)
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+   ;;
+esac
+#
+### // stage1 ###
+;;
+'freenas')
+### stage1 // ###
+case $OSVERSION in
+FreeBSD)
+### stage2 // ###
+#
+### // stage2 ###
+
+### stage3 // ###
+if [ "$MYNAME" = "root" ]
+then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] You must be root to run this script"
+   exit 1
+fi
+if [ "$JAILED" = "0" ]
+then
+   : # dummy
+else
+   : # dummy
+   : # dummy
+   echo "[ERROR] Run this script on the FreeBSD HOST"
+   exit 1
+fi
+### stage4 // ###
+#
+### ### ### ### ### ### ### ### ###
+
+### FreeNAS // ###
+#
+if [ "$FREENAS" = "1" ]
+then
+   : # dummy
+fi
+#
+### // FreeNAS ###
+
+
+
+### ### ### ### ### ### ### ### ###
+cleanup
+### ### ### ### ### ### ### ### ###
+echo "" # printf
+printf "\033[1;31measy_dtrace for FreeNAS finished.\033[0m\n"
+### ### ### ### ### ### ### ### ###
+#
+### // stage4 ###
+#
+### // stage3 ###
+#
+### // stage2 ###
+   ;;
+*)
+   # error 1
+   : # dummy
+   : # dummy
+   echo "[ERROR] Plattform = unknown"
+   exit 1
+   ;;
+esac
+#
+### // stage1 ###
+;;
+*)
+printf "\033[1;31mWARNING: easy_dtrace is experimental and its not ready for production. Do it at your own risk.\033[0m\n"
+echo "" # usage
+echo "usage: $0 { freebsd | freenas }"
+;;
+esac
+exit 0
+### ### ### PLITC ### ### ###
+# EOF
